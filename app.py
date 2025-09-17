d9cf3f7cd0ac9ef267ac8cf55c71c26191fdca40
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2025-09-17 23:17:01 +0600

    landing page and block html element

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def landing():
+    return render_template("landing.html")
+
+@app.route("/profile")
 def student_profile():
     return render_template(
         "studentProfile.html",

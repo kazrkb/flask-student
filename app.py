ec6f763836a332f43b0c9ec6bbda8ccbb89ea844
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2025-09-17 21:22:50 +0600

    created app pthon with logic

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def student():
-    return ""
+    return render_template(
+        "profile.html",
+        name="Korim",
+        is_topper = "True",
+        subjects = ["Math", "Physics", "Chemistry"]
+        
+    )
